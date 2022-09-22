680de19bf4f9420b944b5d4461619ca9d1d6a836
tami <xorud7979@gmail.com>
2022-09-22 14:31:58 +0900

    feat: 'datas' 파일을 불러와 map돌려서 화면에 보여주기

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,19 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import datas from "../libs/datas.json";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+// function createData(id, name, calories, fat, carbs, protein) {
+//   return { id, name, calories, fat, carbs, protein };
+// }
 
-const rows = [
-  createData("요거트", 159, 6.0, 24, 4.0),
-  createData("아이스크림 샌드위치", 237, 9.0, 37, 4.3),
-  createData("에끌레어", 262, 16.0, 24, 6.0),
-  createData("컵케이크", 305, 3.7, 67, 4.3),
-  createData("진저브레드", 356, 16.0, 49, 3.9),
-];
+// const rows = [
+//   createData(1, "요거트", 159, 6.0, 24, 4.0),
+//   createData(2, "아이스크림 샌드위치", 237, 9.0, 37, 4.3),
+//   createData(3, "에끌레어", 262, 16.0, 24, 6.0),
+//   createData(4, "컵케이크", 305, 3.7, 67, 4.3),
+//   createData(5, "진저브레드", 356, 16.0, 49, 3.9),
+// ];
 
 export default function BasicTable() {
   return (
@@ -25,26 +26,28 @@ export default function BasicTable() {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>음식 (100g serving)</TableCell>
-              <TableCell align="right">칼로리</TableCell>
-              <TableCell align="right">지방&nbsp;(g)</TableCell>
-              <TableCell align="right">탄수화물&nbsp;(g)</TableCell>
-              <TableCell align="right">단백질&nbsp;(g)</TableCell>
+              <TableCell>ID</TableCell>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Gender</TableCell>
+              <TableCell>University</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {datas.map((data) => (
               <TableRow
-                key={row.name}
+                key={data.id}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
-                <TableCell component="th" scope="row">
-                  {row.name}
+                <TableCell acomponent="th" scope="row">
+                  {data.id}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell>{data.first_name}</TableCell>
+                <TableCell>{data.last_name}</TableCell>
+                <TableCell>{data.email}</TableCell>
+                <TableCell>{data.gender}</TableCell>
+                <TableCell>{data.university}</TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -53,11 +56,11 @@ export default function BasicTable() {
       <style>
         {`
     .css-11xur9t-MuiPaper-root-MuiTableContainer-root {
-      width: 800px;
+      width: 900px;
       margin: 0 auto;
     }
     .css-1q1u3t4-MuiTableRow-root {
-      background: tomato;
+      background: #B7C4CF;
     }
     `}
       </style>
